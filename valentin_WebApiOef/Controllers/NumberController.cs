2322653e02f75b0821de560fc7ea971e3087db5e
Valentin Claes <valentinclaes@live.be>
2021-02-23 15:04:31 +0100

    init

---
+++ valentin_WebApiOef/Controllers/NumberController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace valentin_WebApiOef.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumberController : ControllerBase
+    {
+        public Path path = new Path();
+        public NumberController()
+        {
+
+        }
+        [HttpGet]
+        public ActionResult<int> GetNumber()
+        {
+            try
+            {
+                string file = System.IO.File.ReadAllText($"{path.GetFilePath()}getal.txt");
+                return Ok(file);
+            }
+            catch (FileNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+        [HttpPost("create")]
+        public ActionResult CreateNumber(string newNumber)
+        {
+            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(path.GetFilePath(), "getal.txt")))
+            {
+                outputFile.WriteLine(newNumber);
+                return base.Ok();
+            }
+        }
+        [HttpPost("rng")]
+        public ActionResult CreateRngNumber()
+        {
+            var rng = new Random();
+            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(path.GetFilePath(), "getal.txt")))
+            {
+                outputFile.WriteLine(rng.Next(1, 10));
+                return base.Ok();
+            }
+        }
+    }
+}
---
+++ valentin_WebApiOef/Path.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace valentin_WebApiOef
+{
+    public class Path
+    {
+        public string PathName { get; set; }
+        public Path()
+        {
+            string PathName = GetFilePath();
+        }
+        public string GetFilePath()
+        {
+            string path = Directory.GetCurrentDirectory();
+            path = System.IO.Path.GetFullPath(System.IO.Path.Combine(path, @"txt\"));
+            return path;
+        }
+    }
+}
